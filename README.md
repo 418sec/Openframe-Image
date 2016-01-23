b5c4df6e268b0c09d31dc8579786bc02bc9090ae
Jonathan Wohl <jon@jonwohl.com>
2016-01-23 12:58:42 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+> NOTE: this repo is under development. The contents is likely to change significantly.
+
+# Openframe Image
+
+An Openframe FORMAT plugin which enables display of static images.
+
+This is one of the default FORMATs.
+
+For info on developing your own plugins, see the [Openframe-PluginExample](https://github.com/OpenframeProject/Openframe-PluginExample) repo.
---
+++ plugin.js
@@ -0,0 +1,46 @@
+
+var plugin = module.exports = {};
+
+// Do we need this? Are there other plugin-y config things?
+plugin.config = {
+    platform: 'rpi'
+};
+
+// TODO: should this be a constant, supplied by a base module?
+plugin.type = 'FORMAT';
+
+/**
+ * If this plugin is adding a new artwork format, the format definition
+ * should be included as a 'format' property on the plugin object.
+ *
+ * Each format must have a unique name.
+ *
+ * @type {Object}
+ */
+plugin.format = {
+    'name': 'image-fbi',
+    'display_name': 'Image (JPG, PNG)',
+    'download': true,
+    'start_command': 'sudo fbi -a --noverbose -T 1 $filepath',
+    'end_command': 'sudo pkill -f fbi',
+    'tags': ['image']
+};
+
+/**
+ * Plugin initialization method
+ *
+ * Called when the plugin (and its dependencies) have been installed.
+ *
+ * TODO: This will likely get passed a sandboxed API object rather than the full frame controller...
+ *
+ * @param  {object} fc A reference to the frame controller
+ */
+plugin.init = function(fc) {
+    // do your plugin thing
+    console.log('=======>   PluginExample initialized!   <=======');
+};
+
+// TODO - Do we want this? Or can we rely on the deps from package.json?
+plugin.dependencies = {
+  // 'openframe-gpio': 'git+https://git@github.com/OpenframeProject/Openframe-PluginExample.git'
+};
