bfb136cf7cff6e68ff78e9a9bf2dca10eb10cb46
Jonathan Wohl <jon@jonwohl.com>
2016-04-19 09:09:37 -0400

    New extension structure

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 var pjson = require('./package.json'),
     debug = require('debug')('openframe:image'),
-    extension = module.exports = {};
+    Extension = require('openframe-extension');
 
 /**
  * Extension initialization method.
@@ -9,29 +9,20 @@ var pjson = require('./package.json'),
  *
  * @param  {object} OF An interface provided to extensions giving limited access to the frame environment
  */
-extension.init = function(OF) {
-    // do your extension thing
-    debug('=======>   Openframe-Image initialized!   <=======');
 
-    /**
-     * Extensions can add new artwork formats to the frame.
-     *
-     * Each format must have a unique name, which should correspond to the
-     * name of the npm package.
-     */
-    OF.addFormat(
-        {
-            // the name should be the same as the package name
-            'name': pjson.name,
-            // this is what might get displayed to users
-            'display_name': 'Image (JPG, PNG)',
-            // does this type of artwork need to be downloaded to the frame?
-            'download': true,
-            // how do start this type of artwork? currently two token replacements, $filepath and $url
-            'start_command': 'sudo fbi --noverbose -T 1 $filepath',
-            // how do we stop this type of artwork?
-            'end_command': 'sudo pkill -f fbi'
-        }
-    );
-};
-
+module.exports = new Extension({
+    format: {
+        // the name should be the same as the package name
+        'name': pjson.name,
+        // this is what might get displayed to users
+        'display_name': 'Image (JPG, PNG)',
+        // does this type of artwork need to be downloaded to the frame?
+        'download': true,
+        // how do start this type of artwork? currently two token replacements, $filepath and $url
+        'start_command': function(config) {
+            return 'sudo fbi --noverbose -T 1 $filepath';
+        },
+        // how do we stop this type of artwork?
+        'end_command': 'sudo pkill -f fbi'
+    }
+});
